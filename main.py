d12e5c6788eb5fdd36e198ae297a57bb978f43f0
7770hassan-crypto <7770hassan@gmail.com>
2026-06-08 11:17:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Milaaf AI H1 is active and running!")
+import telebot
+
+# ضع هنا التوكن الخاص بك بين علامات التنصيص
+TOKEN = '8692993746:AAG-A3Cp7jt-toEyC5a5kWrfe8FwyduXlF0
+'
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "مرحباً يا أبا عامر، ميلاف جاهز للعمل!")
+
+print("Milaaf AI is running...")
+bot.infinity_polling()

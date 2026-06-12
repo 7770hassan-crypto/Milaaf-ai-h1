16f7b12a8eb910468fe59192fad11e98420a683e
7770hassan-crypto <7770hassan@gmail.com>
2026-06-13 02:42:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+import os
 import telebot
-# ... بقية الـ imports
+from flask import Flask
+import threading
 
-bot = telebot.TeleBot('8692993746:AAFNiVzogov6a7KJUHzRkpiBUffh-GXZenw')
+TOKEN = '8692993746:AAFNiVzogov6a7KJUHzRkpiBUffh-GXZenw'
+bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
 
-# هذا السطر يقتل أي اتصال قديم فوراً
+# نغلق أي ويب هوك قديم
 bot.remove_webhook()
 
-# ... ثم بقية الكود
+@app.route('/')
+def home():
+    return "Milaaf-ai is running!"
+
+# دالة تشغيل البوت
+def run_bot():
+    print("البوت يبدأ الآن...")
+    bot.infinity_polling(none_stop=True)
+
+if __name__ == '__main__':
+    # تشغيل البوت في الخلفية
+    t = threading.Thread(target=run_bot)
+    t.start()
+    
+    # تشغيل Flask على المنفذ الصحيح الذي يتوقعه رندر
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

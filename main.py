08aaf03b5fe38a598358bed3e005bbb3b2432e86
7770hassan-crypto <7770hassan@gmail.com>
2026-06-14 15:18:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,7 @@
-import os
-import telebot
-from flask import Flask
-import threading
-
-TOKEN = '8692993746:AAFNiVzogov6a7KJUHzRkpiBUffh-GXZenw'
-bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
-
-# نغلق أي ويب هوك قديم
-bot.remove_webhook()
-
-@app.route('/')
-def home():
-    return "Milaaf-ai is running!"
-
-# دالة تشغيل البوت
-def run_bot():
-    print("البوت يبدأ الآن...")
-    bot.infinity_polling(none_stop=True)
-
+# بدلاً من تشغيله بالطريقة العادية، استخدم هذا الكود:
 if __name__ == '__main__':
-    # تشغيل البوت في الخلفية
-    t = threading.Thread(target=run_bot)
-    t.start()
+    # هذا السطر يقتل أي اتصال قديم معلق بتليجرام
+    bot.remove_webhook()
     
-    # تشغيل Flask على المنفذ الصحيح الذي يتوقعه رندر
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # skip_pending=True هي التي ستنهي الـ 409
+    bot.infinity_polling(none_stop=True, skip_pending=True)
